ad6f8bc791cc367a1d5c6ca442aa06f6bb837aba
GordonRodseth <72191487+GordonRodseth@users.noreply.github.com>
2021-09-17 16:23:45 +0200

    update

--- root/js/story.js
+++ root/js/story.js
@@ -4,24 +4,42 @@ $(function() {
     $(".chapter").hide();
     $(".movepiece").hide();
     $(".chapter-one").show();
+    $(".qna").hide();
+    setTimeout(function(){$(".qna").show();},2000);
 
     $(".movespiece").on("click", function(){
         $(".movepiece").hide();
         if(chapnum===1){
             $(".chapter-one").show();
+            $(".qna").hide();
+            setTimeout(function(){$(".qna").show();},2000);
         }
         if(chapnum===2){
             $(".chapter-two").show();
+            $(".qna").hide();
+            setTimeout(function(){$(".qna").show();},2000);
+            
         }
         if(chapnum===3){
             $(".chapter-three").show();
+            $(".qna").hide();
+            setTimeout(function(){$(".qna").show();},2000);
         }
         if(chapnum===4){
             $(".chapter-four").show();
+            $(".qna").hide();
+            setTimeout(function(){$(".qna").show();},2000);
         }
         if(chapnum===5){
             $(".chapter-five").show();
+            $(".qna").hide();
+            setTimeout(function(){$(".qna").show();},2000);
         }
+    })
+
+    $(".wrong").on("click", function(){
+        $("p").css("color", "red");
+        setTimeout(function(){$("p").css("color", "black");},500);
     })
 
     $(".chap1answer").on("click", function(){
