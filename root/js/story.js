4525f8cfa267cab41d550e2d4dce5974be99b34a
GordonRodseth <72191487+GordonRodseth@users.noreply.github.com>
2021-09-17 14:53:44 +0200

    added story

---
+++ root/js/story.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    console.log("running")
+
+    $(".chapter-one").show();
+    $(".chapter-two").hide();
+    $(".chapter-three").hide();
+    $(".chapter-four").hide();
+    $(".chapter-five").hide();
+})
